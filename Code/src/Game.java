c9119afb1aaa288221781b286dbbbd73447c31f0
Arsh Verma <arshverma@iiitd.ac.in>
2018-11-01 22:03:03 +0530

    Static GUI implemented. Animations initialized

--- Code/src/Game.java
+++ Code/src/Game.java
@@ -1,16 +1,29 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import static javafx.scene.paint.Color.YELLOW;
 
 
 public class Game extends Application {
 
-    Stage window;
-    BorderPane layout;
+    static int dx = 1;
+    static int dy = 1;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,26 +31,32 @@ public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window= primaryStage;
-        window.setTitle("Snake vs Block: Main Menu");
-
-
-        Menu Menu1= new Menu("Options");
-
-        Menu1.getItems().add(new MenuItem("New Game"));
-        Menu1.getItems().add(new MenuItem("Restart Game"));
-        Menu1.getItems().add(new MenuItem("Resume Game"));
-
-        layout = new BorderPane();
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
+        primaryStage.setTitle("SnakeVsBlock");
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
+        Group ballGroup= new Group();
+        //Parent root = loader.load();
+        Scene scene = new Scene(ballGroup, 500,700, Color.BLACK);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        addSnake(scene);
         primaryStage.show();
-
-        //Scene scene = new Scene(layout, 400, 300);
-        //window.setScene(scene);
-        //window.show();
     }
-}
+
+    private void addSnake(Scene scene) {
+        Rectangle Block= new Rectangle(100,100, YELLOW);
+
+        Group snake1= (Group) scene.getRoot();
+
+        snake1.getChildren().add(Block);
+
+                TranslateTransition transition1 = new TranslateTransition();
+
+                transition1.setNode(Block);
+
+                transition1.setByY(-100);
+                transition1.setDuration(Duration.millis(3000));
+                transition1.play();
+
+            }
+    }
+
---
+++ Code/src/Snake.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.shape.*;
+
+import java.util.ArrayList;
+
+public class Snake {
+    ArrayList<Circle> SnakeBody = new ArrayList<Circle>();
+
+    public void Snake () {
+        Circle head = new Circle();
+    }
+}
