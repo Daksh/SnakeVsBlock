9b8c8ba3ef56a167a3cfd8a3e9e68de9b24b2b68
Daksh Shah <daksh17336@iiitd.ac.in>
2018-11-03 22:35:42 +0530

    Fixing the text on blocks

--- Code/src/Block.java
+++ Code/src/Block.java
@@ -12,36 +12,35 @@ import static javafx.scene.paint.Color.*;
 public class Block {
     protected void addBlock(Scene scene) {
         Rectangle Block1= new Rectangle(100,100, YELLOW);
-        Block1.setX(0);
         Text text1 = new Text("Lorem");
         StackPane stack1 = new StackPane();
         stack1.getChildren().addAll(Block1, text1);
+        stack1.setLayoutX(0);
 
         Rectangle Block2= new Rectangle(100,100, RED);
-        Block2.setX(100);
-        Text text2 = new Text("Lorem");
+        Text text2 = new Text("Ipsum");
         StackPane stack2 = new StackPane();
         stack2.getChildren().addAll(Block2, text2);
+        stack2.setLayoutX(100);
 
         Rectangle Block3= new Rectangle(100,100, BLUE);
-        Block3.setX(200);
-        Text text3 = new Text("Ipsum");
+        Text text3 = new Text("Dolor");
         StackPane stack3 = new StackPane();
         stack3.getChildren().addAll(Block3, text3);
-
+        stack3.setLayoutX(200);
 
         Rectangle Block4= new Rectangle(100,100, GREEN);
-        Block4.setX(300);
-        Text text4 = new Text("Dolor");
+        Text text4 = new Text("Sit");
         StackPane stack4 = new StackPane();
         stack4.getChildren().addAll(Block4, text4);
+        stack4.setLayoutX(300);
 
 
         Rectangle Block5= new Rectangle(100,100, ORANGE);
-        Block5.setX(400);
-        Text text5 = new Text("Sit");
+        Text text5 = new Text("Amet");
         StackPane stack5 = new StackPane();
         stack5.getChildren().addAll(Block5, text5);
+        stack5.setLayoutX(400);
 
 
         Group block1= new Group();
@@ -49,8 +48,8 @@ public class Block {
         block1.getChildren().add(stack1);
         block1.getChildren().add(stack2);
         block1.getChildren().add(stack3);
-        block1.getChildren().add(Block4);
-        block1.getChildren().add(Block5);
+        block1.getChildren().add(stack4);
+        block1.getChildren().add(stack5);
         block1.setLayoutY(-800);
 
 
