9859136141d7a6a913c9544507d87aee54a4e6b9
Arsh Verma <arshverma@iiitd.ac.in>
2018-11-15 12:57:28 +0530

    Add button click functionality to Home button on leaderboard page.

--- Code/src/LeadCtrl.java
+++ Code/src/LeadCtrl.java
@@ -1,9 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LeadCtrl {
-    public Button homebtn;
+    private Button homebtn;
+    @FXML
+    public void handleHomeBtnClick() throws IOException {
+        Stage stage = (Stage) homebtn.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
 
-    public void handleHomeBtnClick() {
-
+        stage.setScene(new Scene(root));
     }
 }
+
