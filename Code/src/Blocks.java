22d37d6008113baad460b1cb3f0442df76628ae5
Daksh Shah <daksh17336@iiitd.ac.in>
2018-12-03 19:26:10 +0530

    Removing LoadData function from Blocks (it was moved to Leaderboard Class

--- Code/src/Blocks.java
+++ Code/src/Blocks.java
@@ -339,29 +339,6 @@ public class Blocks implements Serializable{
         return (int) _anotherBlockGroup.getLayoutY();
     }
 
-	public void LoadData () {
-		LeadCtrl.data.add(new User("Arsh", 1243));
-		LeadCtrl.data.add(new User("Arsh", 1220));
-		LeadCtrl.data.add(new User("Arsh", 1203));
-		LeadCtrl.data.add(new User("Arsh", 1197));
-		LeadCtrl.data.add(new User("Arsh", 1170));
-		LeadCtrl.data.add(new User("Arsh", 1146));
-		LeadCtrl.data.add(new User("Daksh", 1123));
-		LeadCtrl.data.add(new User("Arsh", 1102));
-		LeadCtrl.data.add(new User("Daksh", 1101));
-		LeadCtrl.data.add(new User("Arsh", 1094));
-		LeadCtrl.data.add(new User("Arsh", 1090));
-		LeadCtrl.data.add(new User("Arsh", 1090));
-		LeadCtrl.data.add(new User("Arsh", 1056));
-		LeadCtrl.data.add(new User("Arsh", 1033));
-		LeadCtrl.data.add(new User("Daksh", 1011));
-		LeadCtrl.data.add(new User("Arsh", 1002));
-		LeadCtrl.data.add(new User("Arsh", 1001));
-		LeadCtrl.data.add(new User("Arsh", 994));
-		LeadCtrl.data.add(new User("Daksh", 983));
-		LeadCtrl.data.add(new User("Arsh", 945));
-	}
-
 	public void explosion (double x, double y){
 		
 	}
