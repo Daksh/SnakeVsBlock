019eca01b18118933f905d3a208a6184182f8a8a
Daksh Shah <daksh17336@iiitd.ac.in>
2018-12-03 19:25:31 +0530

    Adding two new classes DataStore and Leaderboard, yet to make them functional

---
+++ Code/src/DataStore.java
@@ -0,0 +1,2 @@
+public class DataStore {
+}
---
+++ Code/src/Leaderboard.java
@@ -0,0 +1,41 @@
+/**
+ * Singleton Design Pattern
+ */
+public class Leaderboard {
+
+    private static Leaderboard board = new Leaderboard();
+
+    private Leaderboard(){
+
+    }
+
+    public static Leaderboard getInstance(){
+        if(board == null)
+            board = new Leaderboard();
+        return board;
+    }
+
+    public static void loadData() {
+        LeadCtrl.data.add(new User("Arsh", 1243));
+        LeadCtrl.data.add(new User("Arsh", 1220));
+        LeadCtrl.data.add(new User("Arsh", 1203));
+        LeadCtrl.data.add(new User("Arsh", 1197));
+        LeadCtrl.data.add(new User("Arsh", 1170));
+        LeadCtrl.data.add(new User("Arsh", 1146));
+        LeadCtrl.data.add(new User("Daksh", 1123));
+        LeadCtrl.data.add(new User("Arsh", 1102));
+        LeadCtrl.data.add(new User("Daksh", 1101));
+        LeadCtrl.data.add(new User("Arsh", 1094));
+        LeadCtrl.data.add(new User("Arsh", 1090));
+        LeadCtrl.data.add(new User("Arsh", 1090));
+        LeadCtrl.data.add(new User("Arsh", 1056));
+        LeadCtrl.data.add(new User("Arsh", 1033));
+        LeadCtrl.data.add(new User("Daksh", 1011));
+        LeadCtrl.data.add(new User("Arsh", 1002));
+        LeadCtrl.data.add(new User("Arsh", 1001));
+        LeadCtrl.data.add(new User("Arsh", 994));
+        LeadCtrl.data.add(new User("Daksh", 983));
+        LeadCtrl.data.add(new User("Arsh", 945));
+    }
+
+}
